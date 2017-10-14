c479e097d0e091a4bb754c7d3e152515e95156ff
Miguel Beltrán Sanz <miguelbeltransanz@gmail.com>
2017-10-14 21:23:53 +0200

    Fix seed

--- test/seed.sh
+++ test/seed.sh
@@ -1,5 +1,7 @@
 docker-compose run contracts node deploy.js "A test blog"
-curl -svX 'POST' -F "image=@./test/a_post.json" http://localhost:5002/ipfs/
-curl -svX 'POST' -F "image=@./test/another_post.json" http://localhost:5002/ipfs/
+a_post=`cat test/a_post.json`
+curl -svX 'POST' --data "$a_post" http://localhost:5002/ipfs/
+another_post=`cat test/another_post.json`
+curl -svX 'POST' --data "$another_post" http://localhost:5002/ipfs/
 docker-compose run contracts node publish.js 0x3240c5bac4d98855f6c32b1182942bd7d91cd7f0 QmPKWPDpxHvP7QmAtpCu6siPPMXZbdcLT3qj1JpmwAMRUf
-docker-compose run contracts node publish.js 0x3240c5bac4d98855f6c32b1182942bd7d91cd7f0 QmVHmyK7ZtucV6PQjN7mT2oq75EYXsnaynP3ykLT41PoC1
+docker-compose run contracts node publish.js 0x3240c5bac4d98855f6c32b1182942bd7d91cd7f0 QmeEjpN2LVDX7NkcxqtXNgJtNpoEvySBpBFxMXRJz1Dyny
